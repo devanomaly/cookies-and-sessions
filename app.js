0b5b028da2841ac8bcc31c4b2fab94621f1562a0
devanomaly <rafaelfpcosta@gmail.com>
2021-08-04 16:43:09 -0300

    brincando com login

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Rafa',
+          email: 'rafa@test.com',
           cart: {
             items: []
           }
