09170fe30df2756d06a0f3829d83aa1a329d9cbe
devanomaly <rafaelfpcosta@gmail.com>
2021-08-04 19:15:57 -0300

    autenticação meia boca ok

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// TODO: lembre como usar o session e MongoDB...
+const session = require("express-session")
+const MongoDBStore = require("connect-mongodb-session")(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+// seguindo a documentação:
+// The MongoDBStore class has 3 required options:
+  // uri: a MongoDB connection string
+  // databaseName: the MongoDB database to store sessions in
+  // collection: the MongoDB collection to store sessions in
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "sessions", // nome da coleção
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,15 +32,22 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:"um segredo top da balada demais pae",
+  resave: false,
+  saveUninitialized: false,
+  store: store //aqui eh onde usamos o MongoDBStore definido mais para cima...
+}))
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5bab316ce0a7c75f783cb8a8')
+//     .then(user => {
+//       req.user = user;
+//       // TODO: store user in session object after login
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use(authRoutes);
 app.use('/admin', adminRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,28 @@
+const User = require("../models/user")
 exports.getLogin = (req, res, next) => {
+  // console.log(req.get("Cookie"))
+  // const isLoggedIn = req.get("Cookie").split("=")[1] === "true"
+  const isLoggedIn = !!req.session.isLoggedIn
+  console.log(!!isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-    // LEMBRE depois que essa resposta foi passada, a request MORREU!!!
-    // Daí vem a necessidade de cookies e etc (;
-  };
+  // req.isLoggedIn = true;
+  // setando cookie
+  // res.setHeader("Set-Cookie", "loggedIn=true")  
+  User.findById('6109e8bdc9de9759a8d7d0c2')
+    .then(user => {
+      req.session.user = user;
+      // TODO: store user in session object after login
+      req.session.isLoggedIn = true 
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+  // LEMBRE depois que essa resposta foi passada, a request MORREU!!!
+  // Daí vem a necessidade de cookies e etc (;
+};
